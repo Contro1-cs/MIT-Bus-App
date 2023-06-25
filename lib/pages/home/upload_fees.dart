e36a7f4bb5fa17ee1ba873d3fc21f684f9a6c4df
Aaditya Jagdale <81344234+Contro1-cs@users.noreply.github.com>
2023-06-25 16:03:27 +0530

    upload file

--- lib/pages/home/upload_fees.dart
+++ lib/pages/home/upload_fees.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:mit_bus_app/lists/lists.dart';
+import 'package:mit_bus_app/pages/home/home.dart';
 import 'package:mit_bus_app/pages/landing_page.dart';
 import 'package:mit_bus_app/widgets/custom_snackbars.dart';
 import 'package:mit_bus_app/widgets/custom_texts.dart';
@@ -19,33 +22,75 @@ class UploadFeesDoc extends StatefulWidget {
 }
 
 TextEditingController _transactionID = TextEditingController();
+bool _uploading = false;
 
 class _UploadFeesDocState extends State<UploadFeesDoc> {
   String _payment = paymentType[0];
+  final storageRef = FirebaseStorage.instance.ref();
 
   File? image;
 
-  Future pickImage() async {
-    try {
-      XFile? image = await ImagePicker().pickImage(source: ImageSource.gallery);
-      if (image == null) errorSnackbar(context, 'Faild to pick image');
-      final receipt = File(image!.path);
-      setState(() {
-        this.image = receipt;
-      });
-      successSnackbar(context, 'Image picked successfully');
-    } on PlatformException catch (e) {
-      errorSnackbar(context, e.toString());
-    }
+  @override
+  void initState() {
+    image = null;
+    _transactionID.text = '';
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    image = null;
+    _transactionID.text = '';
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     var w = MediaQuery.of(context).size.width;
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    final User user = auth.currentUser!;
+    final uid = user.uid;
+
+    Future pickImage() async {
+      try {
+        XFile? image =
+            await ImagePicker().pickImage(source: ImageSource.gallery);
+        if (image == null) errorSnackbar(context, 'Faild to pick image');
+        final receipt = File(image!.path);
+        setState(() {
+          this.image = receipt;
+        });
+      } on PlatformException catch (e) {
+        errorSnackbar(context, e.toString());
+      }
+    }
+
+    Future<void> uploadImageToFirebaseStorage() async {
+      // Get the local file path
+      String filePath = image!.path;
+      File file = File(filePath);
+
+      try {
+        // Create a storage reference from your Firebase app
+
+        // Create a reference to the image file
+        var imageRef = storageRef.child("$uid/${_transactionID.text}.png");
+
+        // Upload the file to Firebase Storage
+        await imageRef.putFile(file);
+
+        successSnackbar(context, 'Image uploaded successfully!');
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => HomePage()));
+      } catch (e) {
+        errorSnackbar(context, 'Something went wrong: ${e.toString()}');
+      }
+    }
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
+        centerTitle: true,
         toolbarHeight: 80,
         title: Text(
           "Fee Receipt",
@@ -57,7 +102,7 @@ class _UploadFeesDocState extends State<UploadFeesDoc> {
         ),
         backgroundColor: const Color(0xff202020),
         elevation: 0,
-        automaticallyImplyLeading: false,
+        automaticallyImplyLeading: true,
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -80,6 +125,29 @@ class _UploadFeesDocState extends State<UploadFeesDoc> {
                     _payment = value!;
                   },
                   title: 'Payment method'),
+              const SizedBox(height: 20),
+              Visibility(
+                visible: image != null,
+                child: Container(
+                  width: w,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: purple,
+                  ),
+                  margin: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 15),
+                    child: SizedBox(
+                      height: 200,
+                      width: 200,
+                      child: image == null
+                          ? const CircularProgressIndicator()
+                          : Image.file(image!),
+                    ),
+                  ),
+                ),
+              ),
               const SizedBox(height: 20),
               Container(
                 width: w,
@@ -143,50 +211,6 @@ class _UploadFeesDocState extends State<UploadFeesDoc> {
                         ),
                 ),
               ),
-              const SizedBox(height: 20),
-              Visibility(
-                visible: image != null,
-                child: Container(
-                  width: w,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    color: purple,
-                  ),
-                  margin: const EdgeInsets.symmetric(horizontal: 25),
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 15),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          height: 200,
-                          width: 200,
-                          child: image == null
-                              ? const CircularProgressIndicator()
-                              : Image.file(image!),
-                        ),
-                        const SizedBox(height: 5),
-                        Text(
-                          image == null
-                              ? '0 MB'
-                              : 'Size: ${(image!.lengthSync() / (1024 * 1024)).toStringAsFixed(2)} MB',
-                          style: GoogleFonts.inter(
-                            textStyle: TextStyle(
-                              color: (image!.lengthSync() / (1024 * 1024)) <= 10
-                                  ? Colors.white
-                                  : Colors.red,
-                              fontSize: 12,
-                              fontWeight: FontWeight.normal,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              )
             ],
           ),
           Container(
@@ -196,6 +220,10 @@ class _UploadFeesDocState extends State<UploadFeesDoc> {
               onPressed: () {
                 if (_transactionID.text.trim().isEmpty) {
                   errorSnackbar(context, 'Upload Transaction ID');
+                } else if (image == null) {
+                  errorSnackbar(context, 'Please add an image');
+                } else {
+                  uploadImageToFirebaseStorage();
                 }
               },
               style: ElevatedButton.styleFrom(backgroundColor: purple),
